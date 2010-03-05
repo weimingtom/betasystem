235fd7aa782d58b7ee499b217d43b37261f29ec4
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-03-05 14:13:18 +0000

    キャラクターの情報を保持するクラスを作成。

---
+++ trunk/Project/Test/src/Character.java
@@ -0,0 +1,18 @@
+
+/**
+    キャラクター情報を保持するクラス.
+*/
+class Character
+{
+    String m_name;
+    int m_hp;
+    int m_attack;
+
+    Character( String name , int hp , int attack )
+    {
+        m_name = name ;
+        m_hp = hp ;
+        m_attack = attack ;
+    }
+};
+
