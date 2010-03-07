9274d61dffb2e01c5b9d371b1813ecd51a9d84ea
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-03-07 05:49:18 +0000

    仮バトルを作成。 :memo Switchで分ける記述はやはり保守性が低いのを実感したので、 SceneBattle内でもState毎のクラスを作成する必要はあるかもしれない。

--- trunk/Project/Test/src/Princess/SceneBattle.java
+++ trunk/Project/Test/src/Princess/SceneBattle.java
@@ -4,15 +4,63 @@ import com.nttdocomo.ui.Graphics;
 class SceneBattle extends SceneBase
 {
     Graphics g = getGraphics();
+    InputInfo m_input = new InputInfo();
+    
     Character m_player = new Character( "ƒvƒŠƒ€" , 10 , 3 );
     Character m_enemy = new Character( "ƒXƒ‰ƒCƒ€" , 5 , 1 );
+    
+    final int State_PlayerTurn =    0 ;
+    final int State_EnemyTurn =     1 ;
+    final int State_Win =           2 ;
+    final int State_Lose =          3 ;
+    
+    int m_state = State_PlayerTurn;
     
     public void Init()
     {
     }
+    
     public void Update()
     {
+        m_input.Update();
+        
+        switch( m_state )
+        {
+        case State_PlayerTurn:
+            System.out.println("player turn");
+            if( m_input.KeyPressedFrame( Display.KEY_SELECT ) == 1 )
+            {
+                m_enemy.m_hp -= m_player.m_attack;
+                if( m_enemy.m_hp <= 0 )
+                {
+                    m_state = State_Win;
+                }else{
+                    m_state = State_EnemyTurn;
+                }
+            }
+            break;
+            
+        case State_EnemyTurn:
+            System.out.println("enemy turn");
+            m_player.m_hp -= m_enemy.m_attack;
+            if( m_player.m_hp <= 0 )
+            {
+                m_state = State_Lose;
+            }else{
+                m_state = State_PlayerTurn;
+            }
+            break;
+            
+        case State_Win:
+            SceneChangeManager.getInstance().ChangeScene( SceneChangeManager.Scene_Battle );
+            break;
+            
+        case State_Lose:
+            
+            break;
+        }
     }
+    
     public void Draw()
     {
         g.lock();
@@ -31,5 +79,10 @@ class SceneBattle extends SceneBase
         g.drawString("m_attack:"+character.m_hp, x , y +=10  );
     }
     
+    public void processEvent( int type , int param )
+    {
+        m_input.UpdateInputInfo( type , param );
+    }
+     
     public void paint( Graphics g ){}
 }
