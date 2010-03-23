21bcf34785d38463cb4cc4e0a6ef29214b27fa6d
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-03-23 01:48:44 +0000

    マップに表示される、ステージクラスを作成。

---
+++ trunk/Project/Test/src/Princess/Stage.java
@@ -0,0 +1,21 @@
+/**
+    ワールドマップで表示される各フィールドを表現するクラス.
+*/
+class Stage
+{
+    String m_name; //!< フィールド名称.
+    Vector m_monster_list; //!< 出現するモンスターのリスト.
+    
+    /**
+        コンストラクタ.
+    */
+    Stage(
+        String name ,
+        Vector monster_list )
+    {
+        m_name = name;
+        m_monster_list = monster_list;
+    }
+};
+
+}
