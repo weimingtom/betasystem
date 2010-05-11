54d3980993646472afc9963e962c5dc5f5521e14
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-05-11 13:06:06 +0000

    戦闘遷移作成の追加忘れを追加。

---
+++ trunk/Project/Test/src/Princess/SceneBattle/SceneWin.java
@@ -0,0 +1,44 @@
+import com.nttdocomo.ui.Display;
+import com.nttdocomo.ui.Graphics;
+
+class SceneWin extends SceneBase
+{
+    Graphics g = StaticObjects.getGraphicsInstance();
+    InputInfo m_input = StaticObjects.getInputInstance();
+    SceneBattleObjects m_objects;
+    
+    SceneWin( SceneManagerBase scene_manager , SceneBattleObjects objects )
+    {
+        super( scene_manager );
+        m_objects = objects;
+    }
+    
+    public void Init()
+    {
+    }
+    
+    public void Update()
+    {
+        m_input.Update();
+        m_objects.Update();
+        if( m_input.IsTrig( Display.KEY_SELECT ) )
+        {
+            m_objects.EndTurn( BattleSceneManager.Scene_Win );
+        }
+    }
+    
+    public void Draw()
+    {
+        m_objects.Draw();
+        g.drawString("ÅyWinÅz" , 100 , 100 );
+    }
+    
+    public void processEvent( int type , int param )
+    {
+        m_input.UpdateInputInfo( type , param );
+    }
+    
+    public void paint( Graphics g ){}
+    
+}
+
