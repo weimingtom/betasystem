24ef8c23d69169d5a21663d85c905db84a54cf84
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-06-10 07:43:10 +0000

    配列の要素数を取得するマクロを追加。

---
+++ trunk_dxlib/Source/System/ArraySize.hpp
@@ -0,0 +1,7 @@
+#ifndef include_guard_29565032_4ed7_4afd_9a87_f64eec5db8c5
+#define include_guard_29565032_4ed7_4afd_9a87_f64eec5db8c5
+
+#define ARRAY_SIZE(array) (sizeof( array ) / sizeof( array[0] ))
+
+#endif //include_guard_29565032_4ed7_4afd_9a87_f64eec5db8c5
+
