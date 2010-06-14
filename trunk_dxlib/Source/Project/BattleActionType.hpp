74acef87a9269ddba73b02ec803aa4a7d8734b79
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-06-14 08:08:39 +0000

    より適切なファイル名に変更。

---
+++ trunk_dxlib/Source/Project/BattleActionType.hpp
@@ -0,0 +1,14 @@
+#ifndef include_guard_uuid_30c54c41_0c5c_48d3_8c5b_ebf1375379ac
+#define include_guard_uuid_30c54c41_0c5c_48d3_8c5b_ebf1375379ac
+
+enum BattleActionType
+{
+    BattleActionType_Sword,
+    BattleActionType_Shield,
+    BattleActionType_Magic,
+    
+    BattleActionType_Num,
+};
+
+#endif // include_guard_uuid_30c54c41_0c5c_48d3_8c5b_ebf1375379ac
+
