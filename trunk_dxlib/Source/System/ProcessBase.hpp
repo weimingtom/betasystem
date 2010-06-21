b0055e8b824be308b6dacf62fa12865f5df167da
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-06-21 12:09:54 +0000

    処理を抽象化したクラスを作成。

---
+++ trunk_dxlib/Source/System/ProcessBase.hpp
@@ -0,0 +1,17 @@
+#ifndef include_guard_uuid_2fbafdb0_3a27_4c00_8380_6d2c086ded76
+#define include_guard_uuid_2fbafdb0_3a27_4c00_8380_6d2c086ded76
+
+/**
+    ˆ—‚ğ’ŠÛ‰»‚·‚é‚½‚ß‚ÌƒNƒ‰ƒX.
+*/
+class ProcessBase
+{
+public:
+    virtual ~ProcessBase(){}
+    
+public:
+    virtual void Process() = 0;
+};
+
+#endif //include_guard_uuid_2fbafdb0_3a27_4c00_8380_6d2c086ded76
+
