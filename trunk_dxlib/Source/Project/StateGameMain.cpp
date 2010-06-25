458ce1bb769fd9e3dbecb84d12fa55e3b7aa780f
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-06-25 15:42:35 +0000

    文字列配列のコロンを打ち損ねていたことで、assertがおきていたので、 コロンを記述。

--- trunk_dxlib/Source/Project/StateGameMain.cpp
+++ trunk_dxlib/Source/Project/StateGameMain.cpp
@@ -26,8 +26,8 @@ char const* const image_name[ ImageType_Num ] =
 {
     "Resource/Image.png",
     "Resource/Player.png",
-    "Resource/Enemy.png"
-    "Resource/Sword.png"
+    "Resource/Enemy.png",
+    "Resource/Sword.png",
 };
 
 class StateGameMain : public StateBase
