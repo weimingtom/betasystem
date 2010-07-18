de3e95afaacc08c02ad6ead1b90f9c06142e42d0
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-07-18 07:53:03 +0000

    画面上にログを出力するクラスのインターフェースを追加。

---
+++ trunk_dxlib/Source/DxLibWrapper/LogPrinter.hpp
@@ -0,0 +1,25 @@
+#ifndef include_36815a75_e9c1_401e_aa3a_c9f2b62191b7
+#define include_36815a75_e9c1_401e_aa3a_c9f2b62191b7
+
+/**
+    画面上にログを出力するクラス.
+*/
+class LogPrinter
+{
+public:
+    virtual ~LogPrinter(){}
+public:
+    virtual void Print( std::string log ) = 0;
+    virtual void Draw() = 0;
+    
+private:
+    class Impl;
+};
+
+/**
+    生成関数.
+*/
+LogPrinter* new_LogPrinter();
+
+#endif // include_36815a75_e9c1_401e_aa3a_c9f2b62191b7
+
