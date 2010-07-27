3be352fa05dac1d5eba35e20d5881c47022a3fe2
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-07-27 12:58:15 +0000

    ワールドマップで画像を表示するようにした。

--- trunk_dxlib/Source/Project/StateWorldMap.cpp
+++ trunk_dxlib/Source/Project/StateWorldMap.cpp
@@ -3,7 +3,9 @@
 #include "System/StateBase.hpp"
 #include "System/StateManagerBase.hpp"
 #include "DxLibWrapper/MouseInput.hpp"
+#include "DxLibWrapper/Graphics.hpp"
 #include "DxLibWrapper/Color.hpp"
+#include "DxLibWrapper/ImageLoader.hpp"
 #include "Project/ProjectStateManager.hpp"
 
 
@@ -19,12 +21,27 @@ public:
 private:
     std::auto_ptr< MouseInput > m_mouse;
     StateManagerBase& m_project_state_manager;
+    std::auto_ptr< ImageLoader > m_image_loader;
+private:
+    enum ImageType
+    {
+        ImageType_WorldMap,
+        ImageType_Num,
+    };
+    static char const* const m_image_list[ ImageType_Num ];
+};
+
+char const* const StateWorldMap::m_image_list[ ImageType_Num ] =
+{
+    "Resource/WorldMap.png",
 };
 
 StateWorldMap::StateWorldMap( StateManagerBase& project_state_manager )
  : m_mouse( new_MouseInput() )
  , m_project_state_manager( project_state_manager )
+ , m_image_loader( new_ImageLoader( m_image_list , ImageType_Num ) )
 {
+    m_image_loader->Load();
 }
 
 void StateWorldMap::Update()
@@ -38,6 +55,7 @@ void StateWorldMap::Update()
 
 void StateWorldMap::Draw()
 {
+    DrawGraph( 0 , 0 , m_image_loader->ImageHandleOf( m_image_list[ ImageType_WorldMap ] ) );
     DrawFormatString( 0,0, ColorOf(255,255,255) , "ワールドマップ");
 }
 
