1842ddbb3c24c07563df78413dde81e4b3f5cfa4
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2010-09-14 05:17:47 +0000

    敵クラスを追加

---
+++ trunk_dxlib/Source/Project/Enemy.cpp
@@ -0,0 +1,21 @@
+#include "Project/Enemy.hpp"
+#include "DxLibWrapper/Graphics.hpp"
+
+Enemy::Enemy(
+    Vector2 pos ,
+    Vector2 size ,
+    int image_handle ,
+    CharacterStatus status )
+ : CollisionObject( pos , size )
+ , m_status( status )
+ , m_image_handle( image_handle )
+{
+}
+
+void Enemy::Draw() const
+{
+    DrawGraph( m_pos , m_image_handle );
+    DrawCollision();
+}
+
+
