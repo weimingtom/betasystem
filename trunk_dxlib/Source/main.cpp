83b2a289d82d606d82ec951be2873880e0c68829
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2011-01-31 08:06:38 +0000

    mainの処理が微妙に分かりづらいのでコメントで補足.

--- trunk_dxlib/Source/main.cpp
+++ trunk_dxlib/Source/main.cpp
@@ -6,8 +6,13 @@ int WINAPI WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow )
 {
-	if( InitApplication() == ApplicationFailure ) { return ApplicationFailure ; }
+	//アプリケーションを初期化.
+	if( InitApplication() == ApplicationFailure ){
+		return ApplicationFailure;
+	}
+	//アプリケーションのループ.
 	LoopApplication();
+	//アプリケーションの終了処理.
 	return EndApplication();
 }
 
