aa64f20ae7cae187e3c44704f8afbaf5c9876509
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2011-02-05 01:54:16 +0000

    使用していないファイルを削除。

--- trunk_dxlib/Source/Project/State/StateWorldMap.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef include_c32bd7b2_9f76_4bfe_b2a3_89aa548f2605
-#define include_c32bd7b2_9f76_4bfe_b2a3_89aa548f2605
-
-class StateBase;
-class StateManagerBase;
-
-StateBase* new_StateWorldMap( StateManagerBase& project_state_manager );
-
-#endif
