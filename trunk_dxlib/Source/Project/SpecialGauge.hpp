d91c1b5def301e2e520da450f5cd6adc90cf4b1f
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2011-09-27 02:04:02 +0000

    必殺ゲージ管理クラスをインターフェースだけ作成。

---
+++ trunk_dxlib/Source/Project/SpecialGauge.hpp
@@ -0,0 +1,23 @@
+#ifndef include_41221b54_6f0f_4e4d_9b0c_2659ed1c8124
+#define include_41221b54_6f0f_4e4d_9b0c_2659ed1c8124
+
+/**
+	必殺ゲージ管理クラス.
+*/
+class SpecialGauge
+{
+public:
+	SpecialGauge();
+	virtual ~SpecialGauge();
+public:
+	void Add(){ mGaugeNum++; }
+	void Reset(){ mGaugeNum=0; }
+	void Draw() const;
+private:
+	static int const GaugeMax = 5;
+	int mGaugeNum;
+};
+
+
+#endif // include_41221b54_6f0f_4e4d_9b0c_2659ed1c8124
+
