e3142936a71d429299c9da0ea22611dfd533ae06
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-05 17:13:14 +0000

    デバッグトップの矢印が動くところまで作った。

--- trunk/Source/Project/Singleton/SingletonProjectStateManager.cpp
+++ trunk/Source/Project/Singleton/SingletonProjectStateManager.cpp
@@ -1,23 +1,24 @@
 #include "SingletonProjectStateManager.hpp"
 
+#include <memory>
 #include <assert.h>
 #include "System/StateManagerBase.hpp"
 #include "Project/State/StateTitle.hpp"
-#include "Project/State/StateAvaterTest.hpp"
+#include "Project/State/StateDebugTop.hpp"
 
 namespace {
 
 //! シングルトンインスタンス.
-static SingletonProjectStateManager* sInstance = NULL;
+std::auto_ptr<SingletonProjectStateManager> sInstance;
 
 } // namespace unnamed
 
 SingletonProjectStateManager* SingletonProjectStateManager::GetInstance()
 {
-    if( sInstance == NULL ){
-        sInstance = new SingletonProjectStateManager();
+    if( !sInstance.get() ){
+        sInstance.reset( new SingletonProjectStateManager() );
     }
-    return sInstance;
+    return sInstance.get();
 }
 
 StateBase* SingletonProjectStateManager::new_State( State select_index )
@@ -25,7 +26,7 @@ StateBase* SingletonProjectStateManager::new_State( State select_index )
     switch( select_index )
     {
     case State_DebugTop:
-        return new StateTitle();
+        return new StateDebugTop();
     case State_Title:
         return new StateTitle();
     default:
@@ -36,7 +37,7 @@ StateBase* SingletonProjectStateManager::new_State( State select_index )
 
 SingletonProjectStateManager::SingletonProjectStateManager()
 {
-	ChangeState( new_State(State_Title) );
+	ChangeState( new_State(State_DebugTop) );
 }
 
 SingletonProjectStateManager::~SingletonProjectStateManager()
