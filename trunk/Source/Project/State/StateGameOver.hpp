1cfdcc6c8fbd18f777998037c013a5c2d0f76fe1
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-06 02:28:09 +0000

    不要なstateを削除。

--- trunk/Source/Project/State/StateGameOver.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef include_8287796b_fede_4928_8bd1_f3f8f340ed7f
-#define include_8287796b_fede_4928_8bd1_f3f8f340ed7f
-
-class StateBase;
-class StateManagerBase;
-
-StateBase* new_StateGameOver( StateManagerBase& project_state_manager );
-
-#endif
