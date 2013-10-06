81f982ebdb5fa27d41d1692690da6d840d8fdcaf
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-06 02:31:59 +0000

    ゲームメインのステートを追加。

---
+++ Source/Project/State/StateGameMain.hpp
@@ -0,0 +1,16 @@
+﻿#ifndef STATE_GAME_MAIN__
+#define STATE_GAME_MAIN__
+
+#include "System/StateBase.hpp"
+
+class StateGameMain : public StateBase
+{
+public:
+    StateGameMain();
+    ~StateGameMain(){}
+public:
+    void Update();
+    void Draw() const;
+};
+
+#endif // STATE_GAME_MAIN__
