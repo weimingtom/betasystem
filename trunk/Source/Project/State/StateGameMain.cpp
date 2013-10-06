ab08179486fb215972d0a2ab80ee2b9fc2cf88e0
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-06 02:31:59 +0000

    ゲームメインのステートを追加。

---
+++ trunk/Source/Project/State/StateGameMain.cpp
@@ -0,0 +1,19 @@
+﻿#include "StateGameMain.hpp"
+
+#include "Project/Singleton/SingletonInputKey.hpp"
+#include "Project/Singleton/SingletonProjectStateManager.hpp"
+#include "DxLibWrapper/Color.hpp"
+#include "System/Range.hpp"
+
+StateGameMain::StateGameMain()
+{
+}
+
+void StateGameMain::Update()
+{
+}
+
+void StateGameMain::Draw() const
+{
+}
+
