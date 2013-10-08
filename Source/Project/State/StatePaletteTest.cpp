2a744d7c5b8b49c18ae285c1cafd5b8637413f4e
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-08 17:58:58 +0000

    パレットカラー変更テストの実装。 カラー変更できたのを確認した。

--- Source/Project/State/StatePaletteTest.cpp
+++ Source/Project/State/StatePaletteTest.cpp
@@ -4,13 +4,21 @@
 #include "DxLibWrapper/InputMouse.hpp"
 #include "System/StateManagerBase.hpp"
 #include "Project/Singleton/SingletonProjectStateManager.hpp"
+#include "Project/Singleton/SingletonImageLoader.hpp"
 
 StatePaletteTest::StatePaletteTest()
+ : mSoftImageHandle( ProjectImageLoader::SoftImageHandleOf( ProjectImageLoader::ImageType_Avater ) )
+ , mChangedColorImageHandle(0)
 {
+    //パレット書き換え.
+    SetPaletteSoftImage( mSoftImageHandle, 0, 255, 0, 0, 0 ) ;
+    //イメージハンドル作成.
+    mChangedColorImageHandle = CreateGraphFromSoftImage(mSoftImageHandle);
 }
 
 StatePaletteTest::~StatePaletteTest()
 {
+    DeleteGraph(mChangedColorImageHandle);
 }
 
 void StatePaletteTest::Update()
@@ -20,5 +28,6 @@ void StatePaletteTest::Update()
 void StatePaletteTest::Draw() const
 {
     DrawFormatString( 0 , 0 , GetColor(0,255,0) , "パレットテスト");
+    DrawGraph(0,0,mChangedColorImageHandle,TRUE);
 }
 
