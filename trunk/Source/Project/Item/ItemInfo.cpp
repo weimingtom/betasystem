15cb79b4f54c4e36f4adfa7928e6a30da9e94b98
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-09 15:09:06 +0000

    アイテム情報の基礎を作成。

---
+++ trunk/Source/Project/Item/ItemInfo.cpp
@@ -0,0 +1,22 @@
+﻿#include "ItemInfo.hpp"
+
+namespace {
+
+ItemInfo item_info_list[] =
+{
+    {"ロングヘアー",    0,0,Equip_Head},
+    {"ショートヘアー",  0,0,Equip_Head},
+    {"ツインテール",    0,0,Equip_Head},
+    {"ポニーテール",    0,0,Equip_Head},
+    {"セーラー服",      0,0,Equip_Body},
+    {"メイド服",        0,0,Equip_Body},
+};
+
+} // namespace unnamed
+
+
+ItemInfo ItemInfoOf( ItemType item_type )
+{
+    return item_info_list[item_type];
+}
+
---
+++ trunk/Source/Project/Item/ItemInfo.hpp
@@ -0,0 +1,38 @@
+﻿#ifndef ITEM_INFO_HPP__
+#define ITEM_INFO_HPP__
+
+//! アイテムの種類.
+enum ItemType
+{
+	Item_None,
+	Item_Hair0,
+	Item_Hair1,
+	Item_Cloth0,
+	Item_Cloth1,
+};
+
+//! 装備箇所
+enum EquipPosition
+{
+	Equip_None, //!< 装備する場所が無い.
+	Equip_Head,
+	Equip_Hair,
+	Equip_Body,
+	Equip_Option,
+	Equip_Num,
+};
+
+//! アイテム詳細情報
+struct ItemInfo
+{
+	char const* name;
+	int color;
+	int level;
+	EquipPosition equip_pos;
+};
+
+//! アイテム情報の取得.
+ItemInfo ItemInfoOf(ItemType);
+
+
+#endif // ITEM_INFO_HPP__
