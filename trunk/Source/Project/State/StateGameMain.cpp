0609c3502ab695a080b78e3b94dca81e902410a0
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-09 15:11:33 +0000

    不要なファイルの削除。

--- trunk/Source/Project/State/StateGameMain.cpp
+++ trunk/Source/Project/State/StateGameMain.cpp
@@ -1,58 +1,14 @@
 ﻿#include "StateGameMain.hpp"
 
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <time.h>
-#include "Project/PlayerStatus.h"
-#include "Project/KoyakuType.h"
-#include "Project/Singleton/SingletonInputKey.hpp"
-#include "Project/Singleton/SingletonProjectStateManager.hpp"
-#include "Project/Singleton/SingletonImageLoader.hpp"
-
 StateGameMain::StateGameMain()
- : mLogPrinter( new_LogPrinter(100,300) )
 {
-	srand( static_cast<unsigned int>( time(NULL) ) );
 }
 
 void StateGameMain::Update()
 {
-    if( KeyInput()->IsTrig( InputKey::Type_Enter ) )
-	{
-		//抽選処理.
-		int const kOnePlayCoin = 3;
-		mPlayerStatus.coin -= kOnePlayCoin;
-		DrawingResult const drawing_result = GetDrawingResult( mPlayerStatus.drawing_status );
-		mPlayerStatus.coin += CoinOf(drawing_result.koyaku_type);
-		std::stringstream ss;
-		ss << "抽選結果:" << NameOf(drawing_result.koyaku_type) << ":" << drawing_result.is_art 
-		    << "プレイヤー情報:" << mPlayerStatus.coin << ":" << mPlayerStatus.drawing_status ;
-		mLogPrinter->Print(ss.str());
-		
-		mKoyakuList.insert( mKoyakuList.begin() , drawing_result.koyaku_type );
-		if( mKoyakuList.size() > kKoyakuListMax ){
-		    mKoyakuList.pop_back();
-		}
-	}
 }
 
 void StateGameMain::Draw() const
 {
-    int const kIconWidth = 32;
-    int const kIconHeight = 32;
-    int const kDrawX = 400;
-    int const kDrawY = 300;
-    
-	for( std::string::size_type i = 0 ; i < kKoyakuListMax && i < mKoyakuList.size() ; i++ ){
-        DrawRectGraph(
-            kDrawX, kDrawY - i * kIconHeight,
-            0, mKoyakuList.at(i)*kIconHeight,
-            kIconWidth, kIconHeight,
-            ProjectImageLoader::ImageHandleOf(ProjectImageLoader::ImageType_Koyaku),
-            TRUE , FALSE );
-        DrawFormatString( 0 , 0 , GetColor(0,255,0) , "ゲームメイン");
-        mLogPrinter->Draw();
-    }
+    DrawFormatString( 0 , 0 , GetColor(0,255,0) , "ゲームメイン");
 }
--- trunk/Source/Project/State/StateGameMain.hpp
+++ trunk/Source/Project/State/StateGameMain.hpp
@@ -1,12 +1,7 @@
 ﻿#ifndef STATE_GAME_MAIN__
 #define STATE_GAME_MAIN__
 
-#include <memory>
-#include <vector>
 #include "System/StateBase.hpp"
-#include "DxLibWrapper/LogPrinter.hpp"
-#include "Project/PlayerStatus.h"
-#include "Project/DrawingKoyaku.h"
 
 class StateGameMain : public StateBase
 {
@@ -16,11 +11,6 @@ public:
 public:
     void Update();
     void Draw() const;
-private:
-    PlayerStatus mPlayerStatus;
-    std::auto_ptr<LogPrinter> mLogPrinter;
-    std::vector<KoyakuType> mKoyakuList;
-    static int const kKoyakuListMax = 10;
 };
 
 #endif // STATE_GAME_MAIN__
