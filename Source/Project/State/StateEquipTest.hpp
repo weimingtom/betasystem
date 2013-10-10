c1c3df13186fad1ad1e0a59b516effdce08980ec
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-10 15:15:22 +0000

    アバターの表示処理を追加。 髪の毛の色変更などはまだ未実装。

--- Source/Project/State/StateEquipTest.hpp
+++ Source/Project/State/StateEquipTest.hpp
@@ -1,7 +1,9 @@
 ﻿#ifndef STATE_EQUIP_TEST_HPP__
 #define STATE_EQUIP_TEST_HPP__
 
+#include <memory>
 #include "System/StateBase.hpp"
+#include "Project/Avater/Avater.hpp"
 
 class StateEquipTest : public StateBase
 {
@@ -13,6 +15,7 @@ public:
 	void Draw() const;
 private:
     int mIndex;
+    std::auto_ptr<Avater> mAvater;
 };
 
 #endif // STATE_EQUIP_TEST_HPP__
