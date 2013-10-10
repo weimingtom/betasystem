dd4d842c35704473cc4db61ff855cdc2a5a30529
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-10 01:15:41 +0000

    装備テストに選択矢印の追加。

--- Source/Project/State/StateEquipTest.cpp
+++ Source/Project/State/StateEquipTest.cpp
@@ -1,9 +1,11 @@
 ﻿#include "StateEquipTest.hpp"
 
 #include "Project/Singleton/SaveData.hpp"
-#include "System/ArraySize.hpp"
+#include "System/Range.hpp"
+#include "Project/Singleton/SingletonInputKey.hpp"
 
 StateEquipTest::StateEquipTest()
+ : mIndex(0)
 {
     //プレイヤーにいくつかアイテム持たせる。
     gSaveData.mPlayer.mItemList[0] = ItemInfoOf(Item_Hair0);
@@ -19,13 +21,25 @@ StateEquipTest::~StateEquipTest()
 void StateEquipTest::Update()
 {
     //上下でアイテム選択.
+    if( KeyInput()->IsTrig( InputKey::Type_Up ) ){
+        --mIndex;
+    }
+    else if( KeyInput()->IsTrig( InputKey::Type_Down ) ){
+        ++mIndex;
+    }
+    mIndex = Clamp( 0, mIndex, Player::ItemMax - 1 );
+    
     //決定で装備アイテムなら装備.
+    if( KeyInput()->IsTrig( InputKey::Type_Enter ) ) 
+    {
+    }
 }
 
 void StateEquipTest::Draw() const
 {
     DrawFormatString( 0 , 0 , GetColor(0,255,0) , "装備テスト");
     
+    DrawFormatString( 0 , 50+mIndex*15 , GetColor(0,255,0) , "→");
     //所持アイテムの表示.
     for(int i = 0 ; i < Player::ItemMax ; i++ ){
         DrawFormatString( 50 , 50 + i*15 , GetColor(0,255,0) , NameOf( gSaveData.mPlayer.mItemList[i].item_type ) );
