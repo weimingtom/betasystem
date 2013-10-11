b539d7a3142021f83867f08eb8e6a10fb06f9c49
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2013-10-11 16:25:49 +0000

    戦闘画面遷移の追加。

---
+++ trunk/Source/Project/State/StateBattle.cpp
@@ -0,0 +1,46 @@
+﻿#include "StateBattle.hpp"
+
+#include "Project/Singleton/SaveData.hpp"
+#include "Project/Singleton/PlayerParam.hpp"
+#include "System/Range.hpp"
+#include "System/ArraySize.hpp"
+#include "Project/Singleton/SingletonInputKey.hpp"
+
+StateBattle::StateBattle()
+ : mIndex(0)
+{
+}
+
+StateBattle::~StateBattle()
+{
+}
+
+void StateBattle::Update()
+{
+    enum BattleAction{
+        BattleAction_Attack,
+        BattleAction_Pray,
+        BattleAction_Away,
+        BattleAction_Num,
+    };
+    
+    if( KeyInput()->IsTrig( InputKey::Type_Up ) ){
+        --mIndex;
+    }
+    else if( KeyInput()->IsTrig( InputKey::Type_Down ) ){
+        ++mIndex;
+    }
+    mIndex = Clamp( 0, mIndex, BattleAction_Num - 1 );
+    
+    if( KeyInput()->IsTrig( InputKey::Type_Enter ) ) 
+    {
+    }
+}
+
+void StateBattle::Draw() const
+{
+    DrawFormatString( 0 , 0 , GetColor(0,255,0) , "戦闘画面");
+    
+    DrawFormatString( 0 , 50+mIndex*15 , GetColor(0,255,0) , "→");
+}
+
---
+++ trunk/Source/Project/State/StateBattle.hpp
@@ -0,0 +1,18 @@
+﻿#ifndef STATE_BATTLE_HPP__
+#define STATE_BATTLE_HPP__
+
+#include "System/StateBase.hpp"
+
+class StateBattle : public StateBase
+{
+public:
+    StateBattle();
+    ~StateBattle();
+public:
+	void Update();
+	void Draw() const;
+private:
+    int mIndex;
+};
+
+#endif // STATE_BATTLE_HPP__
