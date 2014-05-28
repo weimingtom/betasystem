c0085b35c901006b77fb69abbbf07c62b9a28b17
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-05-28 12:55:57 +0000

    画像表示テストシーケンスを作成

---
+++ Source/Project/State/StateDrawTexture.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef STATE_DRAW_TEXTURE_HPP_
+#define STATE_DRAW_TEXTURE_HPP_
+
+#include "System/StateBase.hpp"
+
+/**
+   画像描画テスト.
+*/
+class StateDrawTexture : public StateBase
+{
+public:
+    StateDrawTexture();
+    ~StateDrawTexture();
+public:
+	void Update();
+	void Draw();
+private:
+    int mIndex;
+};
+
+#endif // STATE_DRAW_TEXTURE_HPP_
+
