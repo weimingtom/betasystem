1d4043f58a481200ea8c2f3c5bc4b217957034d6
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-07-05 23:30:10 +0000

    キャラランダム生成のプロトタイプを作成するステートを作成

---
+++ trunk/Source/Project/State/StateCharacterGenerate.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef state_title_hpp
+#define state_title_hpp
+
+#include "System/StateBase.hpp"
+
+/**
+*/
+class StateCharacterGenerate : public StateBase
+{
+public:
+    StateCharacterGenerate();
+    ~StateCharacterGenerate(){}
+public:
+	void Update();
+	void Draw();
+private:
+    enum Step
+    {
+        Step_Title,
+    };
+private:
+    Step m_step;
+    int m_frame;
+};
+
+#endif
