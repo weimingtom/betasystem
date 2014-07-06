85d5080b370d0411e5a2df74688de1849f4ad835
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-07-06 02:40:43 +0000

    キャラクターステータスランダム生成テストを実装しました。

--- trunk/Source/Project/State/StateCharacterGenerate.hpp
+++ trunk/Source/Project/State/StateCharacterGenerate.hpp
@@ -1,7 +1,8 @@
-﻿#ifndef state_title_hpp
-#define state_title_hpp
+﻿#ifndef _StateCharacterGenerate_
+#define _StateCharacterGenerate_
 
 #include "System/StateBase.hpp"
+#include "DxLibWrapper/Random.hpp"
 
 /**
 */
@@ -13,14 +14,47 @@ public:
 public:
 	void Update();
 	void Draw();
+
 private:
-    enum Step
-    {
-        Step_Title,
+	//! ステータス種別.
+    enum StatusType{
+        StatusType_HP,
+        StatusType_Power,
+        StatusType_Deffence,
+        StatusType_Speed,
+        StatusType_Technique,
+        StatusType_Luck,
+        StatusType_Num,
     };
 private:
-    Step m_step;
-    int m_frame;
+    //! ユニットのステータス
+    class UnitStatus
+    {
+    public:
+        //! コンストラクタ
+        UnitStatus()
+        {
+			for( int i = 0 ; i < StatusType_Num ; i++ ){
+				mStatus[i] = 0;
+			}
+			mID = 0;
+        }
+    public:
+        //! ランダムでステータスを決定する.
+        void SetRandom( int id )
+        {
+        	for( int i = 0 ; i < StatusType_Num ; i++ ){
+        		mStatus[i] = GetRand(5,15);
+        	}
+        	mID = id;
+        }
+    public:
+    	int mStatus[StatusType_Num];
+    	int mID;
+    };
+private:
+    UnitStatus mUnitStatus;
+    int mGenerateNum;
 };
 
 #endif
