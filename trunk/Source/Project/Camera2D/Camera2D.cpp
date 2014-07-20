d34c259dabc041d0e6419f5428ed25e48ec2dfc8
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-07-20 15:32:26 +0000

    カメラ対応

---
+++ trunk/Source/Project/Camera2D/Camera2D.cpp
@@ -0,0 +1,15 @@
+﻿#include "Camera2D.hpp"
+
+#include "Project/Unit/UnitPlayer.hpp"
+
+void Camera2D::Update()
+{
+	SetLookAt( gUnitPlayer().GetPos() );
+}
+
+Camera2D& gCamera2D()
+{
+	static Camera2D sCamera2D;
+	return sCamera2D;
+}
+
--- trunk/Source/Project/Unit/Global.cpp
+++ trunk/Source/Project/Unit/Global.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Project/Unit/UnitPlayer.hpp"
 
+#include "Project/Camera2D/Camera2D.hpp"
 
 static bool gIsVisibleCollision = false;
 
@@ -11,6 +12,25 @@ bool IsVisibleCollision()
 void SetVisibleCollision( bool is_visible )
 {
 	gIsVisibleCollision = is_visible;
+}
+
+void DrawGraphInCamera( int x, int y , ProjectImageLoader::ImageType handle, int touka = TRUE, int hanten = FALSE)
+{
+	if( hanten == FALSE ){
+	    DrawTurnGraph( 
+	    	static_cast<int>( x + gCamera2D().GetDrawOffset().x ),
+	    	static_cast<int>( y + gCamera2D().GetDrawOffset().y ),
+			ProjectImageLoader::ImageHandleOf( handle ),
+			touka
+		);
+	}else{
+	    DrawGraph( 
+	    	static_cast<int>( x + gCamera2D().GetDrawOffset().x ),
+	    	static_cast<int>( y + gCamera2D().GetDrawOffset().y ),
+			ProjectImageLoader::ImageHandleOf( handle ),
+			touka
+		);
+	}
 }
 
 
