b14100ab8af16f99215381af127f173304bdeddf
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-07-21 02:33:50 +0000

    敵バリエーション増加

--- Source/Project/Unit/UnitEnemy.hpp
+++ Source/Project/Unit/UnitEnemy.hpp
@@ -4,14 +4,28 @@
 #include "UnitBase.hpp"
 #include "UnitPlayer.hpp"
 
+//! 敵の種類.
+enum EnemyID{
+	EnemyID_Normal,
+	EnemyID_Speed,
+	EnemyID_HighSpeed,
+	EnemyID_WeightSlime,
+	EnemyID_Num,
+};
+
 //! 敵ユニット.
 class UnitEnemy : public UnitBase
 {
 public:
-	UnitEnemy(){}
+	UnitEnemy() : mEnemyID(EnemyID_Normal) {}
 	~UnitEnemy(){}
 public:
+	void Initialize( EnemyID enemy_id );
 	virtual void Update() override;
+	virtual void Draw() override;
+private:
+	EnemyID mEnemyID;
+	int mMoveSpeed;
 };
 
 #endif // IncludeGuard_UnitEnemy_
