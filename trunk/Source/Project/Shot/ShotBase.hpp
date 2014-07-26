8bec580e3aa24402541e89d6f891823c9e3e5f21
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-07-26 04:30:40 +0000

    追尾弾の実装

--- trunk/Source/Project/Shot/ShotBase.hpp
+++ trunk/Source/Project/Shot/ShotBase.hpp
@@ -4,6 +4,7 @@
 #include "System/Vector2.hpp"
 #include "Project/Camera2D/Camera2D.hpp"
 #include "Project/Unit/Global.hpp"
+#include "Project/Unit/UnitPlayer.hpp"
 
 // ショット
 class ShotBase
@@ -13,6 +14,7 @@ public:
 		: mState( State_None )
 		, mLifeFrame(0)
 		, mSize( Vector2(32,32) )
+		, mIsFollow(false)
 	{
 	}
 public:
@@ -21,12 +23,22 @@ public:
 	*/
 	void Update()
 	{
-		mPos += mSpeed;
+		// 追尾弾
+		if( mIsFollow ){
+			Vector2 target_dir = gUnitPlayer().GetPos() - mPos ;
+			target_dir.Normalize();
+			Vector2 last_dir = mDir * 10 + target_dir;
+			mDir = last_dir.Normalize();
+		}
+
+		mPos += mDir * mSpeed;
+
 		if( mLifeFrame ){ mLifeFrame--; }
 		if( mLifeFrame == 0 ){
 			mState = State_None;
 			mPos = Vector2(0.0f,0.0f);
-			mSpeed = Vector2(0.0f,0.0f);
+			mDir = Vector2(0.0f,0.0f);
+			mSpeed = 0.0f;
 		}
 	}
 	/**
@@ -40,7 +52,7 @@ public:
 	    	static_cast<int>( mPos.x + gCamera2D().GetDrawOffset().x ),
 	    	static_cast<int>( mPos.y + gCamera2D().GetDrawOffset().y ),
 	    	static_cast<int>( 8 ),
-	    	GetColor(0,255,0) , TRUE
+	    	mIsFollow ? GetColor(255,0,0) : GetColor(0,255,0) , TRUE
 		);
 
 		if( IsVisibleCollision() ){
@@ -56,12 +68,15 @@ public:
 	/**
 		初期化.
 	*/
-	void Initialize( Vector2 pos, Vector2 speed )
+	void Initialize( Vector2 pos, Vector2 dir )
 	{
 		mPos = pos;
-		mSpeed = speed * 6;
-		mLifeFrame = 300;
+		mDir = dir;
+		mSpeed = 6.0f;
+		mLifeFrame = 100;
 		mState = State_Shot;
+		
+		mIsFollow = (GetRand(1) == 0);
 	}
 	/**
 		外部から消す.
@@ -90,9 +105,11 @@ private:
 private:
 	Vector2 mPos;
 	Vector2 mSize;
-	Vector2 mSpeed;
+	Vector2 mDir;
+	float mSpeed;
 	State mState;
 	int mLifeFrame;
+	bool mIsFollow;
 };
 
 // ショット管理.
