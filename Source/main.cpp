2727c37c1ba9dc687ebd46fa90c2ee9134305ba6
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2014-08-03 03:38:06 +0000

    FPS表示追加

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,6 +11,29 @@
 #include "Project/Singleton/SingletonInputKey.hpp"
 #include "../Resource/resource.h"
 
+
+//! FPS表示.
+void FpsTimeFanction(){
+
+	static int counter=0,FpsTime[2]={0,},FpsTime_i=0;
+	static double Fps=0.0;
+	static char Key[256];
+
+        if(FpsTime_i== 0)
+                FpsTime[0]=GetNowCount();               //1周目の時間取得
+        if(FpsTime_i==49){
+                FpsTime[1]=GetNowCount();               //50周目の時間取得
+                Fps = 1000.0f / ((FpsTime[1] - FpsTime[0]) / 50.0f);//測定した値からfpsを計算
+                FpsTime_i=0;//カウントを初期化
+        }
+        else
+                FpsTime_i++;//現在何周目かカウント
+        if(Fps != 0)
+                DrawFormatString(565,460,GetColor(255,255,255),"FPS %.1f",Fps); //fpsを表示
+        return;
+}
+
+
 //! シングルトンの初期化.
 void InitSingleton()
 {
@@ -80,6 +103,9 @@ void MainLoopOfApplication()
         
         //Draw
 		Princess::ProjectStateManager::GetInstance()->Draw();
+
+		FpsTimeFanction();
+
         ScreenFlip() ;    // 裏画面の内容を表画面に反映させる
     }
 }
