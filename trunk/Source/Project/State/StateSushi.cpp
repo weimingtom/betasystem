cc288196e318884d5a411f7d1d364323d5e2d3fa
gshion203 <gshion203@0828d2ce-0bba-11df-9175-8d1c77adf3cb>
2015-06-14 13:11:37 +0000

    スコア制 スライム数の動的変化対応

--- trunk/Source/Project/State/StateSushi.cpp
+++ trunk/Source/Project/State/StateSushi.cpp
@@ -10,22 +10,33 @@ namespace Game
 {
 	StateSushi::StateSushi()
 	 : mIndex(0)
+	 , mScore(0)
 	{
+		Character chara;
+		mCharaList.push_back( chara );
+		mCharaList.push_back( chara );
+		mCharaList.push_back( chara );
 	}
 
 	void StateSushi::Update()
 	{
+		// 増える
+//		if( mScore >= 1000 && mCharaList.size() == 1 ){
+//			Character chara;
+//			mCharaList.push_back( chara );
+//		}
+	
 		// 上下
 	    if( KeyInput()->IsTrig( InputKey::Type_Up ) ){ mIndex--; }
 	    else if( KeyInput()->IsTrig( InputKey::Type_Down ) ){ mIndex++; }
-        mIndex = Clamp( 0, mIndex, kCharaNum - 1 );
+        mIndex = Clamp( 0, mIndex, mCharaList.size() - 1 );
 
 		// 寿司増やし
 		if( KeyInput()->IsTrig( InputKey::Type_Right ) ){
 			switch_flag = true;
 		}
 		else if( KeyInput()->IsTrig( InputKey::Type_Left ) ){
-			if(switch_flag){
+			if( switch_flag ){
 				switch_flag = false;
 				mFoodStock.push_back( (FoodType)GetRand(FoodType_Num) );
 			}
@@ -34,13 +45,15 @@ namespace Game
 		// エサ食わせ.
 		if( mFoodStock.size() ){
 			if( KeyInput()->IsTrig( InputKey::Type_Enter ) ){
-				mCharaList[mIndex].AddFood( mFoodStock.at(0) );
+				mCharaList.at(mIndex).AddFood( mFoodStock.at(0) );
 				mFoodStock.erase( mFoodStock.begin() );
 			}
 		}
 
-		for( int i = 0 ; i < kCharaNum ; i++ ){
-		    mCharaList[i].Update();
+		for( int i = 0 ; i < mCharaList.size() ; i++ ){
+		    if( mCharaList.at(i).Update() ){
+		    	mScore += 100;
+		    }
 		}
 	}
 	
@@ -57,18 +70,18 @@ namespace Game
 	void StateSushi::Draw()
 	{
 	    SetFontSize(12);
-	    DrawFormatString( 0, 0, ColorOf(0,255,0) , "寿司ゲー");
+	    DrawFormatString( 0, 0, ColorOf(0,255,0) , "寿司ゲー:Score[%d]", mScore );
 	    
 	    for( unsigned int i = 0 ; i < mFoodStock.size(); i++ ){
 			DrawFormatString( i * 20 , 50, GetColor( mFoodStock.at(i) ) , "○");
 	    }
 
-		for( int i = 0 ; i < kCharaNum ; i++ )
+		for( int i = 0 ; i < mCharaList.size() ; i++ )
 		{
-		    DrawFormatString( 0, 100 + i*20 , ColorOf(0,255,0) , "%03d", mCharaList[i].GetFrame() );
+		    DrawFormatString( 0, 100 + i*20 , ColorOf(0,255,0) , "%03d", mCharaList.at(i).GetFrame() );
 
-			for( unsigned int j = 0; j<mCharaList[i].GetFoodList().size() ; j++ ){
-				DrawFormatString( 150+j*20, 100 + i*20 , GetColor( mCharaList[i].GetFoodList().at(j).food_type ) , "●" );
+			for( unsigned int j = 0; j<mCharaList.at(i).GetFoodList().size() ; j++ ){
+				DrawFormatString( 150+j*20, 100 + i*20 , GetColor( mCharaList.at(i).GetFoodList().at(j).food_type ) , "●" );
 			}
 		    
 		    if( mIndex == i){ DrawFormatString( 50, 100 + i*20 , ColorOf(0,255,0) , "→" ); } 
